fc7beadd2f840463342b3f27aa7be74f5f05ac16
rysta <rysta@mail.ru>
2023-10-09 09:54:12 +0300

    add card and grid

---
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibiansScreen.kt
@@ -0,0 +1,89 @@
+package com.example.amphibians.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.amphibians.R
+import com.example.amphibians.model.Amphibian
+
+@Composable
+fun AmphibiansScreen(){
+
+}
+
+@Composable
+fun AmphibiansGrid(amphibians: List<Amphibian>, modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(1),
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(4.dp)
+    )
+    {
+        items(items = amphibians, key = {amphibian -> amphibian.name}){
+            amphibian -> AmphibianCard(
+                amphibian,
+                modifier = modifier.padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
+    }
+}
+
+@Composable
+fun AmphibianCard(amphibian: Amphibian, modifier: Modifier = Modifier){
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Text(
+            text = "${amphibian.name} (${amphibian.type})",
+            fontWeight = FontWeight.Bold
+        )
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(amphibian.imgSrc)
+                .crossfade(true)
+                .build(),
+            contentScale = ContentScale.Crop,
+            contentDescription = amphibian.description,
+//            modifier = modifier.fillMaxWidth(),
+            error = painterResource(R.drawable.ic_broken_image),
+            placeholder = painterResource(R.drawable.loading_img)
+        )
+        Text(text = amphibian.description, modifier = modifier.padding(top = 4.dp))
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AmphibianCardPreview(){
+    val amphibian = Amphibian(
+        name = "Лягушка бык",
+        description = "This toad spends most of its life underground due to the arid desert conditions in which it lives. Spadefoot toads earn the name because of their hind legs which are wedged to aid in digging. They are typically grey, green, or brown with dark spots.",
+        type = "Жаба",
+        imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+    )
+    AmphibianCard(amphibian = amphibian,
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .aspectRatio(1.5f))
+}
